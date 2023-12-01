303debcfb668194bd9050f2d3d6c26d19914e684
smollfly <muhammed.bas@iu-study.org>
2023-12-01 15:46:56 +0100

    events und user werden jetzt ausgegeben (wip)

--- src/Controller/EventzController.php
+++ src/Controller/EventzController.php
@@ -5,14 +5,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Repository\GameNightRepository;
+use App\Form\GameNightType;
+use App\Entity\GameNight;
+#[Route('/eventz')]
 class EventzController extends AbstractController
 {
-    #[Route('/eventz', name: 'app_eventz')]
-    public function index(): Response
+    #[Route('/', name: 'app_eventz')]
+    public function index(GameNightRepository $gameNightRepository): Response
     {
         return $this->render('eventz/index.html.twig', [
-            'controller_name' => 'EventzController',
+            'game_nights' => $gameNightRepository->findAll(),
+        ]);
+
+    }
+    #[Route('/{id}', name: 'app_game_night_show', methods: ['GET'])]
+    public function show(GameNight $gameNight): Response
+    {
+        return $this->render('eventz/show.html.twig', [
+            'game_night' => $gameNight,
         ]);
     }
 }
